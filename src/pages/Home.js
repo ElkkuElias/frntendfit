9af4c0092fcea8a5e1dab9de5c34a16353c85bf3
Soares <soares_123@msn.com>
2023-10-09 19:41:53 +0300

    fix homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,23 @@
 import React from "react";
 import "./Home.css"
 import { Link } from 'react-router-dom';
+
 const Home = () => {
     return (
         <div className="background">
             <div className="photo"></div>
-        <div className="foreground">
-        <h1 className="First_Text">TRACK YOUR, </h1>
-        <h1 className="First_Text">PROGRESS, </h1>
-        <h1 className="First_Text">ACHIEVE YOUR</h1>
-        <h1 className="First_Text">SUCCESS.</h1>
+            <div className="foreground">
+                <h1 className="First_Text">TRACK YOUR,<br />PROGRESS,<br/>ACHIEVE YOUR<br />SUCCESS.</h1>
+            </div>
+            <div className="text-container">
+                <h2 className="Second_Text">CONSTRUCT YOUR IDEAL PROGRAM, <br />TROUGH OUR EXTENSIVE<br/>EXERCISE CATALOGUE.</h2>
+                <div className="button-container">
+                    <Link className="button" to="Signup">Get started!</Link>
+                    <Link className="button" to="Explore">Explore!</Link>
+                </div>
+            </div>
         </div>
-        <div>
-        <h2 className="second">CONSTRUCT YOUR IDEAL PROGRAM,</h2>
-        <h2 className="third">TROUGH OUR EXTENSIVE</h2>
-        <h2 className="fourth">EXERCISE CATALOGUE.</h2>
-        </div>
-        <button className="button">
-              <Link className="links" to="Signup">Get started!</Link>
-        </button>
-        <button className="button2">
-              <Link className="links" to="Explore">Explore!</Link>
-        </button>
-        <div className="blank">O</div>
-        </div>
-        
-        
     )
 }
 
-export default Home
+export default Home;
