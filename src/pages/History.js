2adb4540d3512c635c1f23ac7e505bd22fb968c8
andreydeshko <andydeshko@gmail.com>
2023-10-09 18:36:16 +0300

    remove fav exercises from modal window in history page and session storage

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,4 +1,4 @@
-import { useEffect, useState  } from "react"
+import { useEffect, useState } from "react"
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 import { useAuthContext } from "../hooks/useAuthContext";
 
@@ -18,6 +18,8 @@ const History = () => {
   const [favorites, setFavorites] = useState([])
   const [areFavoritesVisible, setAreFavoritesVisible] = useState(false)
 
+
+
   const retrieveFavorites = () => {
     let favorites = JSON.parse(sessionStorage.getItem('favoriteExercises')) || [];
     setFavorites(favorites);
@@ -26,7 +28,7 @@ const History = () => {
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch(apiUrl, {
-        headers: { 'Authorization' : `Bearer ${user.token}` } // sending auth header with the user's token, which we grab on the bakend to protect API routes
+        headers: { 'Authorization': `Bearer ${user.token}` } // sending auth header with the user's token, which we grab on the bakend to protect API routes
       })
       const allWorkouts = await response.json()
 
@@ -45,6 +47,15 @@ const History = () => {
     retrieveFavorites()
   }, []);
 
+  const handleClick = (index) => {
+    // Remove the item from the favorites array
+    const updatedFavorites = [...favorites];
+    updatedFavorites.splice(index, 1);
+    setFavorites(updatedFavorites);
+    sessionStorage.setItem("favoriteExercises", JSON.stringify(updatedFavorites));
+  };
+
+
   return (
     <div className="history">
       <div className="workouts">
@@ -52,14 +63,14 @@ const History = () => {
           <WorkoutDetails workout={workout} key={workout._id} />
         ))}
       </div>
-      <WorkoutForm 
+      <WorkoutForm
         setAreFavoritesVisible={setAreFavoritesVisible}
         areFavoritesVisible={areFavoritesVisible} />
       {areFavoritesVisible && (
         <div className="modal">
           <div className="favourites">
             <h2>Favourite Exercises</h2>
-            <button 
+            <button
               className="close-button"
               onClick={() => setAreFavoritesVisible(false)}
             >
@@ -71,6 +82,12 @@ const History = () => {
                   <div key={index} className="grid-item">
                     <p>{exercise.name}</p>
                     <img src={exercise.gifUrl} alt={exercise.name} />
+                    <span
+                      className="material-symbols-outlined"
+                      onClick={() => handleClick(index)}
+                    >
+                      delete
+                    </span>
                   </div>
                 ))
               ) : (
